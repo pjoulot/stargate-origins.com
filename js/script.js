e64530ab0a649ff80981d433820ddabab0c0f702
phjou <philippe.joulot@laposte.net>
2018-03-18 03:47:16 +0100

    Add an open/close animation for the mobile menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 (function() {
   document.querySelector('.mobile-menu-button').addEventListener("click", function(){
     this.classList.toggle("open");
+    document.querySelector('.js-overlay').classList.toggle("visible");
+    document.querySelector('.main-menu').classList.toggle("open");
   }, false);
 })();
