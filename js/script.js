1eef7768e8826344dedcb567b40855eb70d240be
phjou <philippe.joulot@laposte.net>
2018-03-18 05:57:38 +0100

    First version of the menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
 (function() {
+  try {  
+    document.createEvent("TouchEvent");
+    hasTouch = true;
+  }
+  catch (e) {
+	  hasTouch = false;
+  }
+  if (!hasTouch) {
+    document.body.className += ' no-touch';
+  }
+
   document.querySelector('.mobile-menu-button').addEventListener("click", function(){
     this.classList.toggle("open");
     document.querySelector('.js-overlay').classList.toggle("visible");
     document.querySelector('.main-menu').classList.toggle("open");
   }, false);
-})();
+  
+  var linksLevel1 = document.querySelectorAll('.main-menu--level-1.main-menu--links .multiple-links>a');
+  
+  for (var i=0; i< linksLevel1.length; i++) {
+    linksLevel1[i].addEventListener("click", function(event){
+      event.preventDefault();
+      this.parentElement.classList.toggle("open");
+      var secondLevel = this.parentElement.querySelector('.main-menu--level-2');
+      var sizeSecondLevel = secondLevel.childElementCount * 63;
+      secondLevel.style.height = sizeSecondLevel + "px";
+      if (!this.parentElement.classList.contains('open')) {
+        this.blur();
+        secondLevel.style.height = "0";
+      }
+
+    }, false);
+  }
+
+  })();
